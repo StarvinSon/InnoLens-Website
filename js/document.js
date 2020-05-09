8d6013b3cc3ca1f664a6f8353656054ae908ae47
Starfish <starfish.lam.1223@gmail.com>
2020-05-09 23:25:17 +0800

    Update document section

--- js/document.js
+++ js/document.js
@@ -1,4 +1,7 @@
 var rippleElements = document.getElementsByClassName('ripple');
+var kitFace = document.getElementsByClassName('supervisor');
+var invisibleText = document.getElementsByClassName('invis');
+var counter = 0;
 
 //Create Ripple Effect
 function createRipple() {
@@ -28,3 +31,12 @@ for (var i = 0; i < rippleElements.length; i++) {
     rippleElements[i].addEventListener('mousedown', createRipple, false);
     rippleElements[i].addEventListener('touchstart', createRipple, false);
 }
+
+$(document).ready(function() {
+    $('.supervisor').click(function() {
+        counter += 1;
+        if (counter === 20) {
+            $('.invis').text("who didn't provide the DreamLab data and lowered our grades");
+        }
+    });
+});
